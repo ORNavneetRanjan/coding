582632162a8da12ee16cd7bebf961a456b718e82
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2024-10-02 13:12:11 +0530

    "dsa"

---
+++ c++/bit manupulation/xor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //check if the 'i'th bit is set or not 
+        
+
+    
+    
+    
+    
+    
+    
+    // swap 2 numbers without using extra space
+        // int a = 10;
+        // int b = 11;
+        // a = a ^ b;
+        // b = a ^ b;
+        // a = a ^ b;
+        // cout<<a<<" "<<b<<endl;
+}
