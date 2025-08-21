78ab7bd86ce04ae819db19344d5fc1b676cbc82f
ORNavneetRanjan <navneet.ranjan274057@gmail.com>
2025-08-21 22:56:26 +0530

    learned heap sort

--- c++/sorting/heapSort.cpp
+++ c++/sorting/heapSort.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void heapify(vector<int>& arr, int length, int index){
+    if(index >= length) return ;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
 
+    int largest = index;
 
+    if(left < length && arr[left] > arr[largest]){
+        largest = left;
+    }
+
+    if(right < length && arr[right] > arr[largest]){
+        largest = right;
+    }
+
+    if(largest != index){
+        swap(arr[index], arr[largest]);
+        heapify(arr, length, largest);
+    }
+}
+
+void heapSort(vector<int>& arr){
+    int n = arr.size();
+    for(int i = n / 2 -1;i>=0;i--){
+        heapify(arr, n, i);
+    }
+
+    for(int i = n - 1;i>=0;i--){
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(vector<int>& arr){
+    for (int i = 0; i < arr.size(); ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+// Driver's code
 int main(){
+    vector<int> arr = { 9, 4, 3, 8, 10, 2, 5 };
 
+    // Function call
+    heapSort(arr);
+
+    cout << "Sorted array is \n";
+    printArray(arr);
 }
